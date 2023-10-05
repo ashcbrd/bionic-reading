85f3ecb37053b3d1dd73a2434ecde1c2d97a45b7
ashcbrd <cabardoash@gmail.com>
2023-10-06 06:29:14 +0800

    docs: Update content of about page

--- app/(sections)/about.tsx
+++ app/(sections)/about.tsx
@@ -6,48 +6,50 @@ const AboutSection = () => {
     >
       <div>
         <h1 className="pb-2 text-xl md:text-5xl font-bold text-transparent bg-gradient-to-tr bg-clip-text from-red-500 to-yellow-500">
-          Enhancing Reading with
-          <br /> Bionic Intelligence
+          About Bioreader
         </h1>
       </div>
       <div className="flex flex-col w-full gap-y-2 md:gap-y-4 items-start">
         <p className="text-sm md:text-xl md:leading-8 text-primary dark:text-stone-200">
-          Welcome to BioReader, where we revolutionize the way you interact with
-          written content through the power of Bionic Reading. Our mission is to
-          significantly enhance your reading experience by improving
-          comprehension, speed, and overall cognitive performance.
+          Welcome to Bioreader, a cutting-edge web application that
+          revolutionizes how you read and experience texts. Bioreader transforms
+          ordinary text into a bionic reading format, allowing you to engage
+          with content in an entirely new and exciting way.
+        </p>
+        <h1 className="pt-2 text-md md:text-3xl font-bold">
+          Embrace Bionic Reading
+        </h1>
+        <p className="text-sm md:text-xl md:leading-8 text-primary dark:text-stone-200">
+          Bioreader leverages advanced technology to infuse a bionic touch into
+          your reading experience. Bionic reading involves merging the
+          efficiency of technology with the natural fluency of reading. Our
+          platform utilizes state-of-the-art algorithms to enhance the reading
+          process, making it more intuitive and efficient.
+        </p>
+        <h1 className="pt-2 text-md md:text-3xl font-bold">
+          Customize Your Bionic Reading Experience
+        </h1>
+        <p className="text-sm md:text-xl md:leading-8 text-primary dark:text-stone-200">
+          <b>• Font Size:</b> Adjust the font size to suit your comfort and
+          preferences, ensuring an effortless reading experience. <br />
+          <b>• Font Color:</b> Tailor the color of the bionic text to enhance
+          visibility and create a visually appealing reading environment.
+          <br /> <b>• Background Color:</b> Personalize the background color to
+          complement the bionic text and optimize readability based on your
+          preferences.
+        </p>
+        <h1 className="pt-2 text-md md:text-3xl font-bold">
+          Join the Bionic Reading Revolution
+        </h1>
+        <p className="text-sm md:text-xl md:leading-8 text-primary dark:text-stone-200">
+          Bioreader is on a mission to redefine how we interact with text. We
+          envision a future where reading is not just a passive activity but an
+          engaging, dynamic experience. With Bioreader, you can be part of this
+          exciting journey into the future of reading.
         </p>
         <p className="text-sm md:text-xl md:leading-8 text-primary dark:text-stone-200">
-          Imagine a world where absorbing vast amounts of information
-          effortlessly is not just a dream, but a reality. At BioReader, we
-          envision a future where every individual can excel academically and
-          professionally by effortlessly navigating the immense sea of written
-          knowledge. Bionic Reading is at the core of this vision—it&apos;s an
-          innovative approach that merges human cognition with advanced
-          algorithms and interactive techniques. This synergy optimizes how you
-          read, comprehend, and retain information, mimicking the natural
-          patterns of your brain&apos;s processing.
-        </p>
-        <p className="text-sm md:text-xl md:leading-8 text-primary dark:text-stone-200">
-          Here&apos;s how BioReader works: You paste the text you want to read
-          into our{" "}
-          <a
-            className="underline underline-offset-2 font-semibold text-orange-400"
-            href="/bionic-reading"
-          >
-            user-friendly interface
-          </a>
-          , and our cutting-edge algorithms analyze the content. We
-          strategically highlight phrases and guide your eye movements to
-          enhance reading speed and comprehension.
-        </p>
-        <p className="text-sm md:text-xl md:leading-8 text-primary dark:text-stone-200">
-          The result? A seamless and efficient reading experience, where you
-          grasp and retain the material with newfound clarity. BioReader is
-          committed to continuously advancing our technology, ensuring you
-          unlock the full potential of your intellect. Join us in this exciting
-          journey to revolutionize reading and discover the true capabilities of
-          your mind with BioReader.
+          We invite you to dive into the world of bionic reading and let
+          Bioreader be your guide to a captivating reading adventure!
         </p>
       </div>
     </div>
